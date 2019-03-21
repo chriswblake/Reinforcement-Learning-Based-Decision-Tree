64ea12c6a51d232c082b6278ffb25b357adade29
Christopher W. Blake <chriswblake@gmail.com>
2019-03-21 16:11:50 -0400

    Added methods for saving chart as a PDF.

--- RLDT.Experiments/Chart.cs
+++ RLDT.Experiments/Chart.cs
@@ -2,6 +2,9 @@
 using System.IO;
 using System.Collections.Generic;
 using System.Text;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
 
 namespace RLDT.Experiments
 {
@@ -21,6 +24,14 @@ namespace RLDT.Experiments
             this.xAxisTitle = xAxisTitle;
             this.yAxisTitle = yAxisTitle;
         }
+
+        //Properties (pdf only)
+        public double width { get; set; } = 800;
+        public double height { get; set; } = 400;
+        public double? xMin { get; set; } = null;
+        public double? xMax { get; set; } = null;
+        public double? yMin { get; set; } = null;
+        public double? yMax { get; set; } = null;
 
         //Methods - Private
         private string ToDataset(string SeriesName)
@@ -71,6 +82,11 @@ namespace RLDT.Experiments
             }
             chartSeriesData[seriesName].Add(new Tuple<double, double>(x, y));
         }
+        public void ToHtml(string address)
+        {
+            if (Path.GetExtension(address).ToLower() != ".html") address = address + ".html";
+            File.WriteAllText(address, ToHtml());
+        }
         public string ToHtml()
         {
             // Get Templates
@@ -90,6 +106,69 @@ namespace RLDT.Experiments
             chartHtml = chartHtml.Replace("datasets: []", datasets);
 
             return chartHtml;
+        }
+        public void ToPdf(string address)
+        {
+            //Create empty chart
+            var myModel = new PlotModel()
+            {
+                Title = this.title,
+                Background = OxyColor.FromArgb(255, 255, 255, 255),
+                PlotAreaBorderColor =OxyColor.FromArgb(25, 0, 0, 0),
+            };
+
+            //Configure X Axis
+            LinearAxis xAxis = new LinearAxis()
+            {
+                Title = this.xAxisTitle,
+                Position = AxisPosition.Bottom,
+                IsAxisVisible = true,
+                MajorGridlineStyle = LineStyle.Solid,
+                MajorGridlineColor = OxyColor.FromArgb(25, 0, 0, 0)
+            };
+            if (xMin.HasValue) xAxis.Minimum = xMin.Value;
+            if (xMax.HasValue) xAxis.Maximum = xMax.Value;
+            myModel.Axes.Add(xAxis);
+
+            //Configure Y Axis
+            LinearAxis yAxis = new LinearAxis()
+            {
+                Title = this.yAxisTitle,
+                Position = AxisPosition.Left,
+                IsAxisVisible = true,
+                MajorGridlineStyle = LineStyle.Solid,
+                MajorGridlineColor = OxyColor.FromArgb(25, 0, 0, 0)
+            };
+            if (yMin.HasValue) yAxis.Minimum = yMin.Value;
+            if (yMax.HasValue) yAxis.Maximum = yMax.Value;
+            myModel.Axes.Add(yAxis);
+
+            //Create series
+            foreach (var seriesData in chartSeriesData)
+            {
+                //Add group
+                var series = new ScatterSeries
+                {
+                    Title = seriesData.Key,
+                    MarkerType = MarkerType.Circle,
+                    MarkerSize = 3,
+                    MarkerFill = OxyColor.FromArgb(255, 0, 255, 0)
+                };
+                //Add data
+                foreach(var datapoint in seriesData.Value)
+                    series.Points.Add(new ScatterPoint(datapoint.Item1, datapoint.Item2));
+
+                //Add to chart
+                myModel.Series.Add(series);
+            }
+
+            //Save chart to file as PDF
+            if (Path.GetExtension(address).ToLower() != ".pdf") address = address + ".pdf";
+            using (var stream = File.Create(address))
+            {
+                var exporter = new PdfExporter { Width = width, Height = height };
+                exporter.Export(myModel, stream);
+            }
         }
     }
 }

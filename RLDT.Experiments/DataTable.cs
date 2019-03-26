52d63d66dbd7f5f3eddb8e50290b53706a1e3ca9
Christopher W. Blake <chriswblake@gmail.com>
2019-03-26 13:51:03 -0400

    Added extension method for the DataTable type to convert to a csv file.

---
+++ RLDT.Experiments/DataTable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace System.Data
+{
+    public static class DataTableExtensions
+    {
+        public static void ToCsv(this DataTable dataTable, string address)
+        {
+            //Adjust file address
+            if (Path.GetExtension(address).ToLower() != ".csv") address = address + ".csv";
+
+            //Create file
+            StreamWriter swCSV = new StreamWriter(address);
+
+            //Get Columns
+            swCSV.WriteLine(String.Join(",", dataTable.Columns.Cast<DataColumn>().Select(p => p.ColumnName)));
+
+            //Get Data
+            foreach (DataRow r in dataTable.Rows)
+                swCSV.WriteLine(String.Join(",", r.ItemArray));
+
+            //Close file
+            swCSV.Close();
+        }
+    }
+}

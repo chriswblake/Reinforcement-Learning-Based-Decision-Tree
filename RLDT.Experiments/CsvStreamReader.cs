abd59d9c12cff3893ac9f5c71b752d6cd7dcdfa3
Christopher W. Blake <chriswblake@gmail.com>
2019-03-27 16:31:11 -0400

    Importance/weights are now optional (default=0). Importance is specified by extra columns in the CSV and can change per row. columnName="<FieldName>:importance"

--- RLDT.Experiments/CsvStreamReader.cs
+++ RLDT.Experiments/CsvStreamReader.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using RLDT;
 
@@ -8,8 +10,10 @@ namespace RLDT.Experiments
     {
         //Fields
         private StreamReader file = null;
-        private string[] headers = null; //first line of csv file
-        private double[] weights = null; //second line of csv file (-1 to +1)
+        private string[] headers = null;
+        private double[] weights = null;
+        private Dictionary<string,int> fieldNames = new Dictionary<string, int>(); //first line of csv file
+        private Dictionary<string,int> fieldImportances = new Dictionary<string, int>(); //first line of csv file
         private int lineCounter = 0;
 
         //Constructor
@@ -18,9 +22,30 @@ namespace RLDT.Experiments
             //Open csv file
             file = new System.IO.StreamReader(csvAddress);
 
-            //Read headers and weights
-            headers = file.ReadLine().Split(',');
-            weights = file.ReadLine().Split(',').Select(double.Parse).ToArray();
+            //Read headers
+            string[] firstRowHeaders = file.ReadLine().Split(',');//.ToDictionary();//.Select((k, v) => new object[] { "hhh", 5 }).ToDictionary();//.ToDictionary(v=> v, (k,v)=> 5);
+            for (int i=0; i<firstRowHeaders.Count(); i++)
+            {
+                string fieldName = firstRowHeaders[i].Trim();
+                int column = i;
+
+                if(fieldName.ToLower().EndsWith(":importance"))
+                {
+                    fieldName = fieldName.Substring(0, fieldName.LastIndexOf(":importance"));
+                    if (!fieldImportances.ContainsKey(fieldName))
+                        fieldImportances.Add(fieldName, column);
+                }
+                else
+                {
+                    if (!fieldNames.ContainsKey(fieldName))
+                        fieldNames.Add(fieldName, column);
+                }
+                    
+            }
+            
+            //Create dictionary of headers with column location
+            //featureNames = headers.Where(p => p.ToLower().IndexOf(":importance") < 0).ToArray();
+            //weights = file.ReadLine().Split(',').Select(double.Parse).ToArray();
         }
 
         //Properties
@@ -45,8 +70,15 @@ namespace RLDT.Experiments
             string[] dataobjects = line.Split(',');
             lineCounter++;
 
-            //Create a data vector from the headers and read data line
-            return new DataVector(headers, dataobjects);
+            //Build datavector
+            DataVector dv = new DataVector();
+            foreach (var fieldNameKVP in fieldNames)
+            {
+                string name = fieldNameKVP.Key;
+                string value = dataobjects[fieldNameKVP.Value];
+                dv.AddFeature(name, value);
+            }
+            return dv;
         }
         public DataVectorTraining ReadLine(string labelFeatureName)
         {
@@ -59,8 +91,19 @@ namespace RLDT.Experiments
             string[] dataobjects = line.Split(',');
             lineCounter++;
 
-            //Create a data vector from the headers and read data line
-            return new DataVectorTraining(headers, dataobjects, weights, labelFeatureName);
+            //Build Datavector
+            DataVectorTraining dvt = new DataVectorTraining();
+            foreach(var fieldNameKVP in fieldNames)
+            {
+                string name = fieldNameKVP.Key;
+                string value = dataobjects[fieldNameKVP.Value];
+                double importance = 0;
+                if (fieldImportances.ContainsKey(name))
+                    double.TryParse(dataobjects[fieldImportances[name]], out importance);
+                dvt.AddFeature(name, value, importance);
+            }
+            dvt.SetLabel(labelFeatureName, dvt[labelFeatureName].Value);
+            return dvt;
         }
         public void Close()
         {

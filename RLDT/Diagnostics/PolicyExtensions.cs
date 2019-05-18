d9080c71e9e69e156f60383ee11568590a1631ca
Christopher W. Blake <chriswblake@gmail.com>
2019-05-18 15:45:59 -0400

    Added interactive version (HTML) of diagnostics info.

--- RLDT/Diagnostics/PolicyExtensions.cs
+++ RLDT/Diagnostics/PolicyExtensions.cs
@@ -42,5 +42,85 @@ namespace RLDT.Diagnostics
             return s.ToString();
         }
 
+        public static string DiagnosticHtml(this Policy thePolicy)
+        {
+            StringBuilder s = new StringBuilder();
+
+            //Front Matter
+            s.AppendLine("<html>");
+            s.AppendLine(@"
+            <head>
+	            <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>
+	            <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script>
+	            <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js' integrity='sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1' crossorigin='anonymous'></script>
+	            <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js' integrity='sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM' crossorigin='anonymous'></script>
+            </head>
+            ");
+            s.AppendLine("<body>");
+
+            //Policy Details
+            s.AppendLine("<b>Policy Details</b>").AppendLine("</br>");
+            s.AppendFormat("Exploration Rate: {0:F3}", thePolicy.ExplorationRate).AppendLine("</br>");
+            s.AppendFormat("Discount Factor: {0:F3}", thePolicy.DiscountFactor).AppendLine("</br>");
+            s.AppendFormat("Parallel Report Updates: {0}", thePolicy.ParallelReportUpdatesEnabled).AppendLine("</br>");
+            s.AppendFormat("Parallel Query Updates: {0}", thePolicy.ParallelQueryUpdatesEnabled).AppendLine("</br>");
+            s.AppendFormat("Queries Limit: {0:F0}", thePolicy.QueriesLimit).AppendLine("</br>");
+            s.AppendLine("</br>");
+
+            //State Details
+            s.AppendFormat("<b>State Details</b> ({0:F0})", thePolicy.StateSpace.Count).AppendLine("</br>");
+            s.AppendLine("<table class='table table-hover'>");
+            s.AppendLine("<tr>");
+            s.AppendLine("" +
+                "<th>ID</th>" +
+                "<th>Features</th>" +
+                "<th>Queries</th>" +
+                "<th>Labels</th>" +
+                "<th>Gini</th>");
+            s.AppendLine("</tr>");
+            foreach (var keyStatePair in thePolicy.StateSpace.OrderBy(p => p.Key))
+            {
+                int stateID = keyStatePair.Key;
+                State theState = keyStatePair.Value;
+                
+                //State Overview
+                s.AppendFormat("<tr data-toggle='collapse' data-target='#ID{0}' class='clickable'>", stateID).AppendLine();
+                s.AppendFormat("<td>{0}</td>", stateID).AppendLine();
+                s.AppendFormat("<td>{0}</td>", theState.Features.Count).AppendLine();
+                s.AppendFormat("<td>{0}</td>", theState.Queries.Count).AppendLine();
+                s.AppendFormat("<td>{0}</td>", theState.Labels.Count).AppendLine();
+                s.AppendFormat("<td>{0:F5}</td>", theState.GiniImpurity).AppendLine();
+                s.AppendLine("</tr>");
+
+                //State Details
+                s.AppendFormat("<tr id='ID{0}' class='collapse'>",stateID).AppendLine();
+                s.AppendLine("<td colspan='5'>");
+                s.AppendFormat("<div id='ID{0}' class='collapse'>", stateID).AppendLine();
+                s.AppendLine(theState.DiagnosticHtml()).AppendLine();
+                s.AppendFormat("</div>", stateID).AppendLine();
+                s.AppendLine("</td>");
+                s.AppendLine("</tr>");
+            }
+            s.AppendLine("</table>");
+
+            //Back Matter
+            s.AppendLine("</body>");
+            s.AppendLine("</html>");
+
+
+            //s.AppendLine("#### Policy ####");
+            //s.AppendFormat("States: {0:F0}", thePolicy.StateSpaceCount).AppendLine();
+            //s.AppendFormat("Exploration Rate: {0:F3}", thePolicy.ExplorationRate).AppendLine();
+            //s.AppendFormat("Discount Factor: {0:F3}", thePolicy.DiscountFactor).AppendLine();
+            //s.AppendFormat("Parallel Report Updates: {0}", thePolicy.ParallelReportUpdatesEnabled).AppendLine();
+            //s.AppendFormat("Parallel Query Updates: {0}", thePolicy.ParallelQueryUpdatesEnabled).AppendLine();
+            //s.AppendFormat("Queries Limit: {0:F0}", thePolicy.QueriesLimit).AppendLine();
+
+            //s.AppendLine();
+
+            return s.ToString();
+        }
+
+
     }
 }
--- RLDT/Diagnostics/StateExtensions.cs
+++ RLDT/Diagnostics/StateExtensions.cs
@@ -72,5 +72,141 @@ namespace RLDT.Diagnostics
             return s.ToString();
         }
 
+        public static string DiagnosticHtml(this State theState)
+        {
+            StringBuilder s = new StringBuilder();
+            int stateID = theState.GetHashCode();
+
+            //Front Matter
+            s.AppendLine(@"
+            <style>
+                .card-body {
+                    margin:0;
+                    padding:0;
+}
+</style>
+");
+            s.AppendLine("<div class='accordion'>");
+
+            #region Features
+            s.AppendLine("<div class='card'>");
+
+            //Card Header
+            s.AppendFormat(@"
+                <div class='card-header'>
+                  <h5 class='mb-0'>
+                    <button class='btn btn-link' type='button' data-toggle='collapse' data-target='#ID{0}-Features'>
+                      Features ({1:F0})
+                    </button>
+                  </h5>
+                </div>
+            ", stateID, theState.Features.Count);
+
+            //Card Body
+            s.AppendFormat("<div id='ID{0}-Features' class='collapse'>", stateID).AppendLine();
+            s.AppendLine("<div class='card-body'>");
+            s.AppendLine("<table class='table'>");
+            s.AppendLine("<tr>");
+            s.AppendLine("<th>Name</th><th>Value</th>");
+            s.AppendLine("</tr>");
+            foreach (var theFeature in theState.Features)
+            {
+                s.AppendLine("<tr>");
+                s.AppendFormat("<td>{0}</td>", theFeature.Name.ToString());
+                s.AppendFormat("<td>{0}</td>", theFeature.Value);
+                s.AppendLine("</tr>");
+            }
+            s.AppendLine("</table>");
+            s.AppendLine("</div>");
+            s.AppendLine("</div>");
+
+            s.AppendLine("</div>");
+            #endregion
+
+            #region Labels
+            s.AppendLine("<div class='card'>");
+
+            //Card Header
+            s.AppendFormat(@"
+                <div class='card-header'>
+                  <h5 class='mb-0'>
+                    <button class='btn btn-link' type='button' data-toggle='collapse' data-target='#ID{0}-Labels'>
+                      Labels ({1:F0})
+                    </button>
+                  </h5>
+                </div>
+            ", stateID, theState.Labels.Count);
+
+            //Card Body
+            s.AppendFormat("<div id='ID{0}-Labels' class='collapse'>", stateID).AppendLine();
+            s.AppendLine("<div class='card-body'>");
+            s.AppendLine("<table class='table'>");
+            s.AppendLine("<tr>");
+            s.AppendLine("<th>Feature Name</th><th>Feature Value</th><th>Reward</th>");
+            s.AppendLine("</tr>");
+            foreach (var labelRewardPair in theState.Labels.OrderByDescending(p => p.Value))
+            {
+                FeatureValuePair theLabel = labelRewardPair.Key;
+                double reward = labelRewardPair.Value;
+
+                s.AppendLine("<tr>");
+                s.AppendFormat("<td>{0}</td>", theLabel.Name);
+                s.AppendFormat("<td>{0}</td>", theLabel.Value);
+                s.AppendFormat("<td>{0:F3}</td>", reward);
+                s.AppendLine("</tr>");
+            }
+            s.AppendLine("</table>");
+            s.AppendLine("</div>");
+            s.AppendLine("</div>");
+
+            s.AppendLine("</div>");
+            #endregion
+
+            #region Queries
+            s.AppendLine("<div class='card'>");
+
+            //Card Header
+            s.AppendFormat(@"
+                <div class='card-header'>
+                  <h5 class='mb-0'>
+                    <button class='btn btn-link' type='button' data-toggle='collapse' data-target='#ID{0}-Queries'>
+                      Queries ({1:F0})
+                    </button>
+                  </h5>
+                </div>
+            ", stateID, theState.Queries.Count);
+
+            //Card Body
+            s.AppendFormat("<div id='ID{0}-Queries' class='collapse'>", stateID).AppendLine();
+            s.AppendLine("<div class='card-body'>");
+            s.AppendLine("<table class='table'>");
+            s.AppendLine("<tr>");
+            s.AppendLine("<th>Relevant Label</th><th>Feature Name</th><th>Feature Value</th><th>Reward</th>");
+            s.AppendLine("</tr>");
+            foreach (var queryRewardPair in theState.Queries.OrderBy(p => p.Key.Label.Value).ThenBy(p => p.Key.Feature.Name).ThenBy(p => p.Key.Feature.Value).ThenByDescending(p => p.Value))
+            {
+                Query theQuery = queryRewardPair.Key;
+                double reward = queryRewardPair.Value;
+                s.AppendLine("<tr>");
+                s.AppendFormat("<td>{0}</td>", theQuery.Label.Value);
+                s.AppendFormat("<td>{0}</td>", theQuery.Feature.Name);
+                s.AppendFormat("<td>{0}</td>", theQuery.Feature.Value);
+                s.AppendFormat("<td>{0:F3}</td>", reward);
+                s.AppendLine("</tr>");
+            }
+            s.AppendLine("</table>");
+            s.AppendLine("</div>");
+            s.AppendLine("</div>");
+
+            s.AppendLine("</div>");
+            #endregion
+
+            //Back Matter
+            s.AppendLine("</div>");
+
+
+            return s.ToString();
+        }
+
     }
 }
